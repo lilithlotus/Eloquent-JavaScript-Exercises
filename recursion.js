f74ab1030480df02dd6255afb3c0f5a77d93ff7a
Jessica Bensch <jessica.bensch11@gmail.com>
2017-10-05 22:55:42 -0400

    Chapter 3 Exercise File

--- recursion.js
+++ recursion.js
@@ -6,7 +6,7 @@ positive whole number is even or odd: 2. Define a recursive
 function isEven corresponding to this description. The function
 should accept a number parameter and return a Boolean. Test it
 on 50 and 75. See how it behaves on -1. Why? Can you think of a
-way to fix this?
+way to fix this? 
 */
 
 function isEven(n) {
