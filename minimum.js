2183496f9d53a1bacea5f7aab52b92a06df7edc6
Jessica Bensch <jessica.bensch11@gmail.com>
2017-10-05 22:55:05 -0400

    Chapter 3 Exercise File

--- minimum.js
+++ minimum.js
@@ -2,7 +2,7 @@
 The previous chapter introduced the standard function Math.min
 that returns its smallest argument. We can do that ourselves now.
 Write a function min that takes two arguments and returns their
-minimum.
+minimum. 
 */
 
 function minimum(x, y) {
